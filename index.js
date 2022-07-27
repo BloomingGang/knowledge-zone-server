7b3a32dc72d10c22909ac5a60c6da4bd6e5aaf4a
BloomingGang <you@example.comknowledgezone404@gmail.com>
2022-07-27 22:38:44 +0600

    added blogs

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app=express()
-const port =process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.drzmd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const booksCollection = client.db("knowledge-zone").collection("books-collection");
+        const blogCollection = client.db("knowledge-zone").collection("blog-collection");
+     app.get('/books', async(req,res)=>{
+        const result = await booksCollection.find().toArray()
+        res.send(result)
+     })
+
+     app.get("/blogs", async (req, res) => {
+        const result = await blogCollection.find().toArray();
+        res.send(result);
+      });
+      
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send("welcome to Knowledge Zone ")
