9e626bdc69c18573aa22ba15dbce6d195a5517e2
BloomingGang <you@example.comknowledgezone404@gmail.com>
2022-08-01 14:59:07 +0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const jwt = require("jsonwebtoken");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
 //middleware
 app.use(cors());
