7a08b179b7b415045fdce37777c01a73a9af47be
kausarahamed <galibhasan517@gmail.com>
2022-08-15 19:32:05 +0600

    kausar

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
     // for user collection (faisal)
 
     const userCollection = client.db("knowledge-zone").collection("users");
+    //rakib
+    //  class one_to_twelve and courses routes database start
+    const classAndCourse = client
+      .db("classes_courses_info")
+      .collection("allClassesCoursesInfo");
+    //  class one_to_twelve and courses routes database end
 
     //get detail for payment
 
@@ -73,6 +79,29 @@ async function run() {
 
     app.get("/books", async (req, res) => {
       const result = await booksCollection.find().toArray();
+      res.send(result);
+    });
+
+    // create api for get class and courses information
+    app.get("/courses/:course", async (req, res) => {
+      const course = req.params.course;
+      console.log(course);
+      const query = { classCourse: course };
+      const result = await classAndCourse.find(query).toArray();
+      res.send(result);
+    });
+    // after click enroll from course or class route
+    app.get("/course/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await classAndCourse.findOne(query);
+      res.send(result);
+    });
+    // delete a course
+    app.delete("/course/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await classAndCourse.deleteOne(query);
       res.send(result);
     });
 
@@ -208,7 +237,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("welcome to Knowledge Zone.aa");
+  res.send("welcome to Knowledge Zone.aa..");
 });
 
 app.listen(port, () => {
